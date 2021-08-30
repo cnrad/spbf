435f327c7873fe85336971ca761496fc294a6fbb
cnrad <83192247+cnrad@users.noreply.github.com>
2021-08-29 21:42:10 -0400

    testing stuff

--- index.py
+++ index.py
@@ -1,7 +1,19 @@
 import threading
 import random
+import requests
 
 def main():
-    print("seed phrase brute force")
+    for i in range(100):
+        with open("words.txt", "r") as file:
+            allText = file.read()
+            words = list(map(str, allText.split('\n')))
+
+            phrase = []
+
+            for j in range(12):
+                phrase.append(random.choice(words))
+
+            print(' '.join(phrase))
+
 
 main()
