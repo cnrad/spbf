b2ed09bc01660c0f6230253e9b5e3edb210d68a1
cnrad <83192247+cnrad@users.noreply.github.com>
2021-08-30 21:45:28 -0400

    interact with web3, get balance from wallet w valid seed phrase

---
+++ index.js
@@ -0,0 +1,36 @@
+import { words } from './wordList.js';
+import { ethers } from "ethers";
+
+const provider = new ethers.getDefaultProvider("homestead", {
+    etherscan: process.env.API_KEY
+});
+
+function genSeedPhrase() {
+    let seedPhrase = []
+    for (let i = 0; i < 12; i++){
+        let number = Math.floor(Math.random() * 2048);
+        seedPhrase.push(words[number]);
+    }
+
+    return seedPhrase.join(' ');
+}
+
+async function main(){
+    let phrase = genSeedPhrase();
+    let result = ethers.utils.isValidMnemonic(phrase);
+
+    if(result === true) {
+
+        let walletMnemonic = ethers.Wallet.fromMnemonic(phrase);
+
+        let wallet = walletMnemonic.connect(provider)
+        let balance = await wallet.getBalance();
+
+        console.log(`Valid Seed Phrase:\n${phrase}`)
+        console.log("Address: " + wallet.address + "\nBalance: " + balance + " ETH\n\n");
+
+        return main();
+    } else return main();
+}
+
+main();
